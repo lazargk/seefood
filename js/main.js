5df6bf27466f19093b067153e725de31fade491d
Gumarov Lazar <gumarov.lazar@gmail.com>
2022-03-19 17:24:39 +0500

    add: header content

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+  $('.header-slider').slick({
+    dots:true,
+    prevArrow:'<button type="button" class="slick-prev"><img src="images/prev.svg" alt="prev"></button>',
+    nextArrow:'<button type="button" class="slick-next"><img src="images/next.svg" alt="next"></button>',
+    // vertical:true,
+    fade:true,
+  })
 });
